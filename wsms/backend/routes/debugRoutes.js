ac985b27aa66236df5da87fc258c259146ea7f11
noxiouswonder <85096215+noxiouswonder@users.noreply.github.com>
2021-07-20 19:16:56 +0530

    Add files via upload

--- wsms/backend/routes/debugRoutes.js
+++ wsms/backend/routes/debugRoutes.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
-const debug = require("../controllers/debugController");
-
 const router = express.Router();
-router.use(cors());
 
-router.post("/", debug.create_post);
-router.get("/", debug.get);
-router.delete("/", debug._delete);
+router.get("/", (req, res) => {
+  res.send("The Website is Working");
+});
 
 module.exports = router;
